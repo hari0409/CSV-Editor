faaf33fcff8bcef317a41a6db8d8c84a932e307c
hari0409 <73524123+hari0409@users.noreply.github.com>
2022-02-05 02:03:59 +0530

    Updated Mongo Url

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule,MongooseModule.forRoot("")],
+  imports: [ConfigModule.forRoot(),UserModule,MongooseModule.forRoot(process.env.MONGODB_URL)],
   controllers: [],
   providers: [],
 })
