dfe2dd928703648962ce636f545348da97f18e0e
hari0409 <73524123+hari0409@users.noreply.github.com>
2022-02-05 01:44:16 +0530

    Update app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,7 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule,MongooseModule.forRoot("mongodb+srv://admin:admin@cluster0.ohemp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")],
+  imports: [UserModule,MongooseModule.forRoot("")],
   controllers: [],
   providers: [],
 })
